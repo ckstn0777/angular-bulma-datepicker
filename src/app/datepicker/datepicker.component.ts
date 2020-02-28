027015fbaaa3fb644e333aa9b41e5fa0181e15d6
ckstn0777 <ckstn0777@naver.com>
2020-02-28 18:02:41 +0900

    v1.0.0
    알아보기 쉽게 수정

--- src/app/datepicker/datepicker.component.ts
+++ src/app/datepicker/datepicker.component.ts
@@ -1,13 +1,12 @@
-import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
-import * as moment from 'moment';
+import { Component, OnInit, Input, Output, EventEmitter } from "@angular/core";
+import * as moment from "moment";
 
 @Component({
-  selector: 'app-datepicker',
-  templateUrl: './datepicker.component.html',
-  styleUrls: ['./datepicker.component.css']
+  selector: "app-datepicker",
+  templateUrl: "./datepicker.component.html",
+  styleUrls: ["./datepicker.component.css"]
 })
 export class DatepickerComponent implements OnInit {
-
   @Input() locale: string;
   @Input() canChangeNavMonthLogic: any;
   @Input() isAvailableLogic: any;
@@ -19,78 +18,93 @@ export class DatepickerComponent implements OnInit {
   gridArr: Array<any> = [];
   selectedDate: any;
 
-  constructor() { }
+  constructor() {}
 
   ngOnInit() {
     moment.locale(this.locale);
-    this.navDate = moment();
-    this.makeHeader();
-    this.makeGrid();     
+    this.navDate = moment(); // 현재 날짜
+    this.makeHeader(); // weekend 설정
+    this.makeGrid(); // day 설정
   }
 
-  changeNavMonth(num: number){
-    if(this.canChangeNavMonth(num)){
-      this.navDate.add(num, 'month');
+  // month 이동시
+  changeNavMonth(num: number) {
+    // true면 이동 가능
+    if (this.canChangeNavMonth(num)) {
+      this.navDate.add(num, "month");
       this.makeGrid();
     }
   }
 
-  canChangeNavMonth(num: number){    
-    if(this.canChangeNavMonthLogic){      
+  // month 이동 가능 여부 확인
+  canChangeNavMonth(num: number) {
+    if (this.canChangeNavMonthLogic) {
       const clonedDate = moment(this.navDate);
-      return this.canChangeNavMonthLogic(num, clonedDate);      
+      return this.canChangeNavMonthLogic(num, clonedDate);
     } else {
       return true;
     }
   }
 
-  makeHeader(){
+  // 일 -월 - 화 - 수 - 목 - 금 - 토 설정하는거임
+  makeHeader() {
     const weekDaysArr: Array<number> = [0, 1, 2, 3, 4, 5, 6];
-    weekDaysArr.forEach(day => this.weekDaysHeaderArr.push(moment().weekday(day).format('ddd')));
+    weekDaysArr.forEach(day =>
+      this.weekDaysHeaderArr.push(
+        moment()
+          .weekday(day)
+          .format("ddd")
+      )
+    ); // Sun, Mon 등으로 포맷팅
   }
 
-  makeGrid(){
+  //
+  makeGrid() {
     this.gridArr = [];
 
-    const firstDayDate = moment(this.navDate).startOf('month');
-    const initialEmptyCells = firstDayDate.weekday();
-    const lastDayDate = moment(this.navDate).endOf('month');
-    const lastEmptyCells = 6 - lastDayDate.weekday();
-    const daysInMonth = this.navDate.daysInMonth();
-    const arrayLength = initialEmptyCells + lastEmptyCells + daysInMonth;
+    // 현재 2월 27일이라고 가정하고 출력값을 보겠습니다
+    const firstDayDate = moment(this.navDate).startOf("month"); // firstDayDate는 2월 1일
+    const initialEmptyCells = firstDayDate.weekday(); // 6 (토요일이라서 6인가?)
+    const lastDayDate = moment(this.navDate).endOf("month"); // lastDayDate는 2월 29일
+    const lastEmptyCells = 6 - lastDayDate.weekday(); // 0 (이때도 토요일이라서 6-6해서 0)
+    const daysInMonth = this.navDate.daysInMonth(); // 2월이 29일까지 있으니까 29아닌감?
+    const arrayLength = initialEmptyCells + lastEmptyCells + daysInMonth; // 35
 
-    for(let i = 0; i < arrayLength; i++){
+    for (let i = 0; i < arrayLength; i++) {
       let obj: any = {};
-      if(i < initialEmptyCells || i > initialEmptyCells + daysInMonth -1){
+      // 첫번째 조건문은 없는날짜. 그니까 2월을 보면 1일 전에 빈 부분있잖아? 그부분인듯
+      if (i < initialEmptyCells || i > initialEmptyCells + daysInMonth - 1) {
         obj.value = 0;
         obj.available = false;
       } else {
-        obj.value = i - initialEmptyCells +1;
-        obj.available = this.isAvailable(i - initialEmptyCells +1);
+        // 이건 진짜 날짜.
+        obj.value = i - initialEmptyCells + 1;
+        obj.available = this.isAvailable(i - initialEmptyCells + 1);
       }
       this.gridArr.push(obj);
     }
   }
 
-  isAvailable(num: number): boolean{
-    if(this.isAvailableLogic){
+  isAvailable(num: number): boolean {
+    if (this.isAvailableLogic) {
       let dateToCheck = this.dateFromNum(num, this.navDate);
       return this.isAvailableLogic(dateToCheck);
     } else {
       return true;
-    }    
+    }
   }
 
-  dateFromNum(num: number, referenceDate: any): any{
+  // 그날이 무슨 날인지를 알려줌.
+  dateFromNum(num: number, referenceDate: any): any {
     let returnDate = moment(referenceDate);
     return returnDate.date(num);
   }
 
-  selectDay(day: any){
-    if(day.available){
+  // 날짜 선택
+  selectDay(day: any) {
+    if (day.available) {
       this.selectedDate = this.dateFromNum(day.value, this.navDate);
       this.emitSelectedDate.emit(this.selectedDate);
     }
   }
-
 }
--- src/app/parent/parent.component.ts
+++ src/app/parent/parent.component.ts
@@ -1,40 +1,41 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
 
-import * as moment from 'moment';
+import * as moment from "moment";
 
 @Component({
-  selector: 'app-parent',
-  templateUrl: './parent.component.html',
-  styleUrls: ['./parent.component.css']
+  selector: "app-parent",
+  templateUrl: "./parent.component.html",
+  styleUrls: ["./parent.component.css"]
 })
 export class ParentComponent implements OnInit {
-
-  locale: string = 'en';
+  // 지역은 기본적으로 en설정
+  locale: string = "en";
   selectedDate: any;
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit() {
-  }
+  ngOnInit() {}
 
-  setSelectedDate(date){
+  // datapicker에서 선택한 date
+  setSelectedDate(date) {
     this.selectedDate = date;
   }
 
-  canChangeMonthLogic(num, currentDate){
-    currentDate.add(num, 'month');
-    const minDate = moment().add(-1, 'month');
-    const maxDate = moment().add(1, 'year');
-    
-    return currentDate.isBetween(minDate, maxDate);
+  // 달변경, 현재날짜
+  canChangeMonthLogic(num, currentDate) {
+    currentDate.add(num, "month");
+    // const minDate = moment().add(-1, "month");
+    const maxDate = moment().add(0, "month");
+
+    // 달 수정시 '오늘'이 '오늘'과 '다음 연도'사이에 있는지 확인하면 false가 반환됩니다
+    return currentDate.isBetween("2019-01-01", maxDate);
   }
 
-  isAvailableLogic(dateToCheck: any){
-    if(dateToCheck.isBefore(moment(), 'day')){
+  isAvailableLogic(dateToCheck: any) {
+    if (dateToCheck.isAfter(moment(), "day")) {
       return false;
-      } else {
-          return true;
-      }
+    } else {
+      return true;
+    }
   }
-
 }
